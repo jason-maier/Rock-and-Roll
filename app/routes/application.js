8a6afb35cd9e59f4fcb5a6f1d3abbd98ddf4a7cb
Jason Maier <jason.maier@fastly.com>
2019-01-07 17:58:59 -0800

    Finished templates and data bindings chapter

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,12 @@ import Route from '@ember/routing/route';
 import EmberObject from '@ember/object';
 import { A } from '@ember/array';
 
+var Song = EmberObject.extend({
+    title: '',
+    band: '',
+    rating: 0
+});
+
 export default Route.extend({
     model() {
         let blackDog = EmberObject.create({
